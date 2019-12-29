b58d196106758fcac82a2060026f7fdc761d407a
fluke1508 <59298298+fluke1508@users.noreply.github.com>
2019-12-29 23:24:45 +0100

    Added support for checkboxes + minor improvements
    Minor changes:
    Fixed UserScript match pattern for non-english languages;
    Slight improvement of user warning texts;
    Improved addEventListener call compatibility;

--- apply_to_all_LOs.js
+++ apply_to_all_LOs.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Mintos Auto
 // @namespace    https://www.mintos.com/
-// @version      0.2
+// @version      0.3
 // @description  Apply Mintos Auto Invest setting of currently active field to all loan originators
 // @author       fluke
-// @match        *://www.mintos.com/en/auto-invest/*
+// @match        *://www.mintos.com/*/auto-invest/*
 // @grant        none
 // ==/UserScript==
 
@@ -15,25 +15,63 @@
   var inp_event = new Event("input");
 
   document.addEventListener("keydown",
-    function (zEvent) {
-      if (zEvent.ctrlKey && zEvent.shiftKey && (zEvent.code === "KeyY" || zEvent.code === "KeyZ")) {
+    function (event) {
+      if (event.ctrlKey && event.shiftKey && (event.code === "KeyY" || event.code === "KeyZ")) {
+        var i;
+        var elements;
         var act = document.activeElement;
+
+        // checkboxes
+        var sel;
+        elements = document.querySelectorAll( ":hover" );
+        for (i=elements.length-1; i>=0; i=i-1){
+          if ( (typeof(elements[i].type) !== "undefined") &&
+               (elements[i].type.toLowerCase() === "checkbox")
+             ){
+            sel = elements[i];
+            break;
+          }
+        }
+        if (typeof(sel) !== "undefined") {
+          if (act.tagName.toLowerCase() === "input") {
+            alert("Too many suitable fields: Active element is INPUT and mouse over checkbox");
+          }
+          else {
+            var id_re_esc = sel.id.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            var id_re = new RegExp(id_re_esc.replace(new RegExp("(.+\\[)([0-9]+)(\\\\].*)"),
+                                                     "$1([0-9]+)$3"
+                                                    ));
+            elements = document.getElementsByTagName("input");
+            for (i=0; i<elements.length; i=i+1){
+              if ( (elements[i].type.toLowerCase() == "checkbox") &&
+                  (id_re.test(elements[i].id))
+                 ){
+                elements[i].checked = sel.checked;
+                elements[i].dispatchEvent(inp_event);
+                elements[i].dispatchEvent(chg_event);
+              }
+            }
+          }
+          return;
+        }
+
+        // text and date fields
         var val = act.value;
         var apply_flt_id_suf = (act.id.substring(act.id.length-flt_id_suf.length) === flt_id_suf);
-        var elements = document.getElementsByClassName(act.className);
+        elements = document.getElementsByClassName(act.className);
         if ( (typeof(act.id) !== "string") ||
              (act.id === "")
            ){
-          alert("Active element has no ID");
+          alert("No suitable field: Active element has no ID");
         }
-        else if (typeof(val) == "undefined") {
-          alert("Active element '"+act.id+"' has no value");
+        else if (typeof(val) === "undefined") {
+          alert("No suitable field: Active element '"+act.id+"' has no value");
         }
         else if (elements.length <= 1) {
-          alert("Found no matches for active element '"+act.id+"', class '"+act.className+"'");
+          alert("No suitable field: Found no matches for active element '"+act.id+"', class '"+act.className+"'");
         }
         else {
-          for (var i=0; i<elements.length; i=i+1){
+          for (i=0; i<elements.length; i=i+1){
             if ( (typeof(elements[i].id) === "string") &&
                  ( (!apply_flt_id_suf) ||
                    (elements[i].id.substring(elements[i].id.length-flt_id_suf.length) === flt_id_suf)
@@ -47,6 +85,7 @@
           }
         }
       }
-    }
+    },
+    false
   );
 })();
